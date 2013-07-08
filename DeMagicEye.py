9dc741c9b3f34edc472529838d397f668b593806
kscottz <kat@sightmachine.com>
2013-07-08 17:19:32 -0400

    patch based stuff looks slightly better, but woder why there is so much noise... going to try a few things and move on

--- DeMagicEye.py
+++ DeMagicEye.py
@@ -3,12 +3,13 @@ import numpy as np
 img = Image('shark.png')
 img = img.scale(0.5)
 # roughly the number of tiles in an image
-repeats = 5
+
 # roughly how far we scan horizontally
-window = img.width/repeats
+window = 100 #int(img.width/repeats)
+repeats = np.floor(img.width/window)
 print "window: {0}".format(window)
 # how big of a signal we convolve 
-samplesz = window / 4
+samplesz = window / 10
 print "sample: {0}".format(samplesz)
 dmap = np.zeros([img.width-window,img.height])
 
@@ -16,29 +17,27 @@ dmap = np.zeros([img.width-window,img.height])
 # proof of concept.
 print (img.width,img.height)
 npgImg = img.getGrayNumpy()
-for vidx in range(0,img.height):
+for vidx in range(1,img.height-1):
     print "row {0}".format(vidx)
     for hidx in range(0,img.width-window):
         #get our sample
-        currentWndw = np.floor(hidx/float(window))
-        if( currentWndw >= repeats-1 ):
-            continue
-
+        #currentWndw = np.floor(hidx/float(window))
+        #if( currentWndw == repeats-1 ):
+        #    break
         sample = npgImg[hidx:hidx+samplesz,vidx]
         vals = []
-        #print "hidx: {0}".format(hidx)
-        #print "currentidx {0} / sz: {1}".format(currentWndw,window)
-        
-        for sidx in range(0,window-samplesz):
-
-            step = int(((currentWndw+1)*window)-(window/2))
-            #print "searching: {0}->{1}".format(step+sidx,step+sidx+samplesz)
-            tester = npgImg[step+sidx:step+sidx+samplesz,vidx]
-            vals.append( np.abs(np.sum(sample-tester)))
-        #print vals
+        #step = int((((currentWndw+1)*window)))
+        for sidx in range(samplesz,window-samplesz):
+            tester0 = npgImg[hidx+sidx:hidx+sidx+samplesz,vidx-1]
+            tester1 = npgImg[hidx+sidx:hidx+sidx+samplesz,vidx]
+            tester2 = npgImg[hidx+sidx:hidx+sidx+samplesz,vidx+1]
+            v0 = np.abs(np.sum(sample-tester0))
+            v1 = np.abs(np.sum(sample-tester1))
+            v2 = np.abs(np.sum(sample-tester2))
+            vals.append(np.min([v0,v1,v2]))
         best = np.where(np.array(vals)==np.min(vals))[0]
         # offset is the hidx of the current window
-        offset = hidx - (currentWndw*window)
+        offset = 0 #hidx - (currentWndw*window)
         dmap[hidx][vidx] = np.abs(best[0]-offset)
         #print dmap
 print dmap                     
